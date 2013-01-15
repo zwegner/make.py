3dc1255ef40824c6f9feb99cc15747d0e8ed9bd8
mjcraighead@gmail.com <mjcraighead@gmail.com@0edd27ab-7b77-43b6-a7d7-3f7b87e4b155>
2013-01-15 16:35:12 +0000

    add simple example
    git-svn-id: svn://localhost/trunk@30 0edd27ab-7b77-43b6-a7d7-3f7b87e4b155

---
+++ example/rules.py
@@ -0,0 +1,14 @@
+def rules(ctx):
+    c_files = ['main.c']
+
+    o_files = []
+    for file in c_files:
+        o_file = '_out/%s%s' % file.replace('.c', '.o')
+        d_file = o_file.replace('.o', '.d')
+        cmd = ['gcc', '-o', o_file, '-c', file, '-MD']
+        ctx.add_rule(o_file, [file], cmd, d_file=d_file)
+        o_files += [o_file]
+
+    exe_file = '_out/hello'
+    cmd = ['gcc', '-o', exe_file] + o_files
+    ctx.add_rule(exe_file, o_files, cmd)
